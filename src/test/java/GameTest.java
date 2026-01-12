3f338ffa3a76158d999fb8e3feae34fe22a5bcb8
basel.obied <basel.obied@fawry.com>
2026-01-13 00:48:20 +0200

    refactor: add setup for game

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,11 +1,18 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GameTest {
 
+    Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
     @Test
     void testGutterGame() {
-        Game game = new Game();
         for (int i = 0; i < 20; i++) {
             game.roll(0);
         }
@@ -14,7 +21,6 @@ public class GameTest {
 
     @Test
     void TestAllOnes() {
-        Game game = new Game();
         for (int i = 0; i < 20; i++) {
             game.roll(1);
         }
