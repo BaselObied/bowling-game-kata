1704583ccfe2b6a233225e558c792e6ec25d37f1
basel.obied <basel.obied@fawry.com>
2026-01-13 00:37:14 +0200

    test: add Game class

---
+++ src/test/java/GameTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+
+    @Test
+    void testGutterGame() {
+        Game game = new Game();
+
+    }
+}
